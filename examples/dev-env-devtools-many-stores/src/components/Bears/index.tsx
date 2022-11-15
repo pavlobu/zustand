f078c3469a01dc6c9cdf765b6abeaccaebacd5cb
Paul <pavlobu@gmail.com>
2022-11-16 00:48:00 +0100

    improve docs, add demo project, ready for first release

--- examples/dev-env-devtools-many-stores/src/components/Bears/index.tsx
+++ examples/dev-env-devtools-many-stores/src/components/Bears/index.tsx
@@ -1,11 +1,15 @@
-import { useBearsStore } from './bear-store'
-import React from 'react'
+import { useBearsStore } from './bear-store';
+import React from 'react';
 
 export const Bears = () => {
-  const bears = useBearsStore((state) => state.bears)
-  const arr = []
+  const bears = useBearsStore((state) => state.bears);
+  const arr = [];
   for (let i = 0; i < bears; i += 1) {
-    arr.push(<span key={i}>🐻</span>)
+    arr.push(<span key={i}>🐻</span>);
   }
-  return <div>{arr}</div>
-}
+  return (
+    <div>
+      {arr}
+    </div>
+  );
+};
